fbd93a535b5620167db4017e88c289b956be0e26
Dhanush CK <dhanushck01@gmail.com>
2025-11-20 23:45:26 +0530

    feat: Created a model for role details

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    # username = models.CharField(max_length=50)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10,  choices=[
+        ("student", "Student"),
+        ("tutor", "Tutor"),
+        ("admin", "Admin")
+                          ])
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+    
