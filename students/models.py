4208eb98bc65fb5656bb2aee1880a40cfe02d54a
Dhanush CK <dhanushck01@gmail.com>
2025-11-21 23:22:00 +0530

    feat: Renamed and added more fields

--- students/models.py
+++ students/models.py
@@ -5,10 +5,16 @@ from accounts.models import UserProfile
 
 class Student(models.Model):
     profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
-    reg_no = models.CharField(max_length=20)
+    username = models.CharField(max_length=100)
+    regno = models.CharField(max_length=20)
     semester = models.IntegerField()
     department = models.CharField(max_length=200)
+    course = models.CharField(max_length=200)
 
     toughness = models.JSONField(default=dict)
     study_hours = models.JSONField(default=dict)
     sleep_hours = models.JSONField(default=dict)
+
+    def __str__(self):
+        user = self.profile.user
+        return f"{user.username} - Student"
