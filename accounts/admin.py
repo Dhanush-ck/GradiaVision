8d115a2c8d3f307dc63aa7acd2c33c5d854b95b4
Dhanush CK <dhanushck01@gmail.com>
2025-11-21 23:10:56 +0530

    feat: Registered new models(Student & UserProfile)

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from students.models import Student
+from accounts.models import UserProfile
 
 # Register your models here.
+
+admin.site.register(Student)
+admin.site.register(UserProfile)
