3daedb8f173290518d2ce2d310ad5f68ded25038
Dhanush CK <dhanushck01@gmail.com>
2025-11-22 23:48:12 +0530

    feat : Added email field and placeholder for password

--- students/forms.py
+++ students/forms.py
@@ -4,4 +4,5 @@ class StudentForm(forms.Form):
     username = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'placeholder': 'Enter your name', 'id': 'username'}) )
     regno = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'placeholder': 'Enter your registration no', 'id': 'regno'}))
     semester = forms.IntegerField(min_value=1, max_value=8, widget=forms.TextInput(attrs={'placeholder': 'Enter your semester', 'id': 'semester'}))
-    password = forms.CharField(max_length=20, widget=forms.PasswordInput(attrs={'id':'password'}))
+    password = forms.CharField(max_length=20, widget=forms.PasswordInput(attrs={'placeholder': 'Enter your password' ,'id':'password'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder':"Enter your email", 'id':'email'}))
