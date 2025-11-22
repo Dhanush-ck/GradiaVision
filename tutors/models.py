a908ed5efd2de2a378b6a2137a3112f1d428c68b
Dhanush CK <dhanushck01@gmail.com>
2025-11-22 23:11:50 +0530

    feat: Created model for Tutor

--- tutors/models.py
+++ tutors/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from accounts.models import UserProfile
 
 # Create your models here.
+
+class Tutor(models.Model):
+    profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+    username = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+    class_charge = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.username} - {self.email} - Tutor"
