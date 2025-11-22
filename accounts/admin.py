7a26ea8fca954dce4e70e7c5f2797c953df9330d
Dhanush CK <dhanushck01@gmail.com>
2025-11-23 00:04:18 +0530

    feat: Registered tutor model to the admin site

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from students.models import Student
 from accounts.models import UserProfile
+from tutors.models import Tutor
 
 # Register your models here.
 
 admin.site.register(Student)
 admin.site.register(UserProfile)
+admin.site.register(Tutor)
