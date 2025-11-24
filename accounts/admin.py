6791516c57fb25486cec40c2cfb7a34e0bbaeb95
Dhanush CK <dhanushck01@gmail.com>
2025-11-24 23:53:09 +0530

    refactor: removed models which are not part of accounts app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
-from students.models import Student
 from accounts.models import UserProfile
-from tutors.models import Tutor
 
 # Register your models here.
 
-admin.site.register(Student)
 admin.site.register(UserProfile)
-admin.site.register(Tutor)
