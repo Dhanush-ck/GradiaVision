4dae66a67c7fc6a324824457aff448ef0e1be005
Dhanush CK <dhanushck01@gmail.com>
2025-11-25 23:03:35 +0530

    feat: create forms.py for handling forms

---
+++ accounts/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class SigninForm(forms.Form):
+    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Enter your email', 'id': 'email'}))
+    password = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'placeholder': 'Enter your password', 'id': 'password'}))
+
+class EmailForm(forms.Form):
+    email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Enter your email', 'id': 'email'}))
