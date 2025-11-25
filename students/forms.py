894d993c1d8b8693c53a685b3ee2be8a8021d905
Dhanush CK <dhanushck01@gmail.com>
2025-11-25 23:23:02 +0530

    feat: add security question and answer field in form

--- students/forms.py
+++ students/forms.py
@@ -6,7 +6,6 @@ class StudentForm(forms.Form):
     semester = forms.IntegerField(min_value=1, max_value=8, widget=forms.TextInput(attrs={'placeholder': 'Enter your semester', 'id': 'semester'}))
     password = forms.CharField(max_length=20, widget=forms.PasswordInput(attrs={'placeholder': 'Enter your password' ,'id':'password'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder':"Enter your email", 'id':'email'}))
+    security_question = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': "Enter the question", 'id': 'security-question'}))
+    security_answer = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'placeholder': 'Enter the answer', 'id': 'security-answer'}))
 
-class StudentSigninForm(forms.Form):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder':"Enter your email", 'id':'email'}))
-    password = forms.CharField(max_length=20, widget=forms.PasswordInput(attrs={'placeholder': 'Enter your password' ,'id':'password'}))
