26af441d2769cdeb2a8559b7e681613475065b5f
Dhanush CK <dhanushck01@gmail.com>
2025-11-25 23:12:09 +0530

    feat: create views for signin, verify email and reset question

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.hashers import check_password
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.auth.forms import PasswordResetForm
+
+from accounts.forms import SigninForm, EmailForm
 
 # # Create your views here.
 
@@ -14,3 +21,97 @@ def select_role(request):
                 print("admin")
 
     return render(request, 'role.html')
+
+def signin(request):
+    if request.method == 'POST':
+        form = SigninForm(request.POST)
+        if form.is_valid():
+            # email = request.POST['email']
+            # password = request.POST['password']
+
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            try:
+                user_obj = User.objects.get(username=email)
+            except User.DoesNotExist:
+                user_obj = None
+                
+            user = authenticate(request, username=email, password=password)
+
+            if user is not None:
+                login(request, user)
+                print("Login successfull")
+
+            if user_obj is  None:
+                error = "Invalid password or email"
+                print('Invalid password or email')
+            else:
+                error = "Incorrect password. Try again. "
+                print('Incorrect password. Try again.')
+
+        return render(request, 'signin.html', {
+            'error': error,
+            'form': form,
+        })
+
+    else:
+        form = SigninForm()
+
+    
+    return render(request, 'signin.html', {
+        'form': form
+    })
+
+def verify_email(request):
+    if request.method == 'POST':
+        form = EmailForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+
+            if not User.objects.filter(username=email).exists():
+                print("Email doesn't exist")
+                error = "Email doesn't exist"
+                return render(request, 'reset_email.html', {
+                    'form': form,
+                    'error': error,
+                })
+            else:
+                request.session['email'] = email
+                return redirect('question_view')
+
+    else:
+        form = EmailForm()
+    return render(request, 'verify_email.html', {
+        'form': form,
+    })
+
+def question_view(request):
+    email = request.session.get('email')
+    user = User.objects.get(username=email)
+
+    if request.method == 'POST':
+        answer = request.POST['answer']
+
+        if not check_password(answer, user.userprofile.security_answer):
+            print("Invalid answer")
+            return render(request, 'question_view.html', {
+                'question': user.userprofile.security_question,
+                'error': "Invalid answer",
+            })
+        
+        print('Password Reset Redirect')
+
+        form = PasswordResetForm({'email': email})
+        if form.is_valid():
+            form.save(
+                request=request,
+                email_template_name='password_reset_email.html',
+                subject_template_name='password_reset_subject.txt',
+            )
+
+            return redirect('/account/reset/done')
+
+    return render(request, 'question_view.html', {
+        'question': user.userprofile.security_question,
+    })
