42fff28ddcaefef0c7450322f6c1a34cc2c0532f
Dhanush CK <dhanushck01@gmail.com>
2025-11-28 21:12:06 +0530

    fix: change text type to password type for password field

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class SigninForm(forms.Form):
     email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Enter your email', 'id': 'email'}))
-    password = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'placeholder': 'Enter your password', 'id': 'password'}))
+    password = forms.CharField(max_length=20, widget=forms.PasswordInput(attrs={'placeholder': 'Enter your password', 'id': 'password'}))
 
 class EmailForm(forms.Form):
     email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Enter your email', 'id': 'email'}))
