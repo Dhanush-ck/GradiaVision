f63e4a7988f20ee05db8574381bd4fddcc3e293d
Dhanush CK <dhanushck01@gmail.com>
2025-11-29 23:33:58 +0530

    feat: add signout view path in url

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,9 +3,9 @@ from accounts import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('role', views.select_role, name='role'),
+    path('role/', views.select_role, name='role'),
     path(
-        "reset/done",
+        "reset/done/",
         auth_views.PasswordResetDoneView.as_view(
             template_name="accounts/password_reset_done.html"
         ),
@@ -13,7 +13,7 @@ urlpatterns = [
     ),
 
     path(
-        "reset/<uidb64>/<token>",
+        "reset/<uidb64>/<token>/",
         auth_views.PasswordResetConfirmView.as_view(
             template_name="accounts/password_reset_confirm.html",
             success_url="account/reset/complete",  
@@ -22,13 +22,14 @@ urlpatterns = [
     ),
 
     path(
-        "reset/complete",
+        "reset/complete/",
         auth_views.PasswordResetCompleteView.as_view(
             template_name="accounts/password_reset_complete.html"
         ),
         name="password_reset_complete",
     ),
-    path('signin', views.signin, name='signin'),
-    path('verify/email', views.verify_email, name='verify_email'),
-    path('question', views.question_view, name='question_view'),
+    path('signin/', views.signin, name='signin'),
+    path('verify/email/', views.verify_email, name='verify_email'),
+    path('question/', views.question_view, name='question_view'),
+    path('signout/', views.signout, name='signout'),
 ]
