d31631283aa9bf1965385c99470165c1aad23a66
Dhanush CK <dhanushck01@gmail.com>
2025-11-29 23:35:53 +0530

    style: change path end

--- students/urls.py
+++ students/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from students import views
 
 urlpatterns = [
-    path('signup', views.signup_page, name="signup"),
-    path('dashboard', views.dashboard, name='dashboard'),
+    path('signup/', views.signup_page, name="signup"),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
