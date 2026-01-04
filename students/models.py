72f392c12264fc69c0c6cf05e9ca64f5229ed259
Dhanush C K <dhanushck01@gmail.com>
2026-01-04 23:10:00 +0530

    feat: create 3 models for storing marklist details

--- students/models.py
+++ students/models.py
@@ -7,7 +7,7 @@ class Student(models.Model):
     profile = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
     username = models.CharField(max_length=100)
     email = models.EmailField(max_length=254)
-    regno = models.CharField(max_length=20)
+    regno = models.CharField(max_length=20, unique=True)
     semester = models.IntegerField()
     department = models.CharField(max_length=200)
     course = models.CharField(max_length=200)
@@ -19,3 +19,45 @@ class Student(models.Model):
     def __str__(self):
         # user = self.profile.user
         return f"{self.regno} - Student"
+    
+class Subject(models.Model):
+    course_code = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=200)
+    credits = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.course_code} - Subject"
+    
+class StudentMark(models.Model):
+    ASSESSMENT_CHOICES = [
+        ('TH', 'Theory'),
+        ('PR', 'Practical'),
+    ]
+
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    semester = models.IntegerField()
+
+    assessment_type = models.CharField(
+        max_length=2,
+        choices=ASSESSMENT_CHOICES
+    )
+
+    cca_max = models.IntegerField()
+    ese_max = models.IntegerField()
+    cca_score = models.IntegerField()
+    ese_score = models.IntegerField()
+    total_max = models.IntegerField()
+    total = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.subject.course_code} - {self.semester} - {self.assessment_type} - {self.student.username}"
+
+class SemesterResult(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    semester = models.IntegerField()
+    sgpa = models.FloatField()
+    total_credits = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.semester} - {self.student.username}"
