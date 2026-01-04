c6e8c5bd337fdbfdc2ee7a153df442a49894d8c7
Dhanush C K <dhanushck01@gmail.com>
2026-01-04 23:10:31 +0530

    feat: registered new models

--- students/admin.py
+++ students/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from students.models import Student
+from students.models import StudentMark
+from students.models import Subject
+from students.models import SemesterResult
 
 # Register your models here.
 
 admin.site.register(Student)
+admin.site.register(StudentMark)
+admin.site.register(Subject)
+admin.site.register(SemesterResult)
