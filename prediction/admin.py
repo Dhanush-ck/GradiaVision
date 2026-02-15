fe21e767058d112a6a6bf4377654627a27636529
Dhanush C K <dhanushck01@gmail.com>
2026-02-15 23:34:43 +0530

    feat: register new models

--- prediction/admin.py
+++ prediction/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from prediction.models import PredictionData
+from prediction.models import Prediction
+
 # Register your models here.
+admin.site.register(PredictionData)
+admin.site.register(Prediction)
