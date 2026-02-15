332642fa4b1ddbb85f0c1d7271c15515dfee84c0
Dhanush C K <dhanushck01@gmail.com>
2026-02-15 23:36:50 +0530

    feat: model path made absolute

--- prediction/predict.py
+++ prediction/predict.py
@@ -1,7 +1,12 @@
+import os
+from django.conf import settings
 import joblib
 import pandas as pd
 
-model = joblib.load("random_forest.pkl")
+MODEL_PATH = os.path.join(settings.BASE_DIR, 'prediction', 'random_forest.pkl')
+# print(MODEL_PATH)
+
+model = joblib.load(MODEL_PATH)
 
 def predict_score(prev_sgpa, avg_sgpa, sgpa_trend, avg_marks, avg_difficulty, avg_study_hours, planned_effort):
     feature_names = [
@@ -25,7 +30,8 @@ def predict_score(prev_sgpa, avg_sgpa, sgpa_trend, avg_marks, avg_difficulty, av
     ]], columns=feature_names)
 
     predicted_sgpa = model.predict(features_df)[0]
-    print("Predicted SGPA:", round(predicted_sgpa, 2))
+    # print("Predicted SGPA:", round(predicted_sgpa, 2))
+    return round(predicted_sgpa, 2)
     
 
 # predict_score(5.70, 5.60, -10.0, 48.0, 4.3, 1.4, 1.0)
