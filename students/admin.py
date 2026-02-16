7363fda233d7b61b4181c511a008715e3f043407
Dhanush C K <dhanushck01@gmail.com>
2026-02-16 23:28:36 +0530

    feat: register new notification model

--- students/admin.py
+++ students/admin.py
@@ -3,6 +3,7 @@ from students.models import Student
 from students.models import StudentMark
 from students.models import Subject
 from students.models import SemesterResult
+from students.models import Notification
 
 # Register your models here.
 
@@ -10,3 +11,4 @@ admin.site.register(Student)
 admin.site.register(StudentMark)
 admin.site.register(Subject)
 admin.site.register(SemesterResult)
+admin.site.register(Notification)
