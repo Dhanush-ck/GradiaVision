f42dcf2d98cc9da445f2f619dc21df22dd68dfa8
Dhanush C K <dhanushck01@gmail.com>
2026-02-16 23:30:39 +0530

    feat: register new academic risk model

--- tutors/admin.py
+++ tutors/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from tutors.models import Tutor
+from tutors.models import AcademicRisk
 
 # Register your models here.
 
 admin.site.register(Tutor)
+admin.site.register(AcademicRisk)
