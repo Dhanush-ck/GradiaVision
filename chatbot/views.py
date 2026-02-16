bd7dc61510e99c8012cc51bcb0b86799f57d6504
Dhanush C K <dhanushck01@gmail.com>
2026-02-16 23:25:04 +0530

    feat: setup function for chatbot reply

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
+
+from django.views.decorators.csrf import csrf_exempt
+
+from chatbot.chatbot_manager import generate_reply
 
 # Create your views here.
+
+def chatbot(request):
+    return render(request, "chatbot/chatbot.html")
+
+@csrf_exempt
+def reply(request):
+    if request.method == "POST":
+        # data = json.loads(request.body)
+        data = json.loads(request.body.decode("utf-8"))
+
+        user_message = data.get("message")
+
+        # your intent logic here
+        reply =generate_reply(user_message)
+
+        return JsonResponse({
+            "reply": reply
+        })
+    return JsonResponse({"error": "Invalid request"})
