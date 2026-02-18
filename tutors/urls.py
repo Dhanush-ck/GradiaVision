022751efdf64d1b2c6ad391cd227408e0a1258ce
Dhanush C K <dhanushck01@gmail.com>
2026-02-18 23:25:15 +0530

    feat: add new view to the path

--- tutors/urls.py
+++ tutors/urls.py
@@ -4,4 +4,5 @@ from tutors import views
 urlpatterns = [
     path('signup/', views.signup_page, name='signup'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('upload/', views.upload, name='upload'),
 ]
