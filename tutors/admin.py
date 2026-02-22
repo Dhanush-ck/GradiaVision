537015b20e379c615fb8f67c95609830e238bc87
Dhanush C K <dhanushck01@gmail.com>
2026-02-22 23:46:26 +0530

    feat: register attendance risk model

--- tutors/admin.py
+++ tutors/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from tutors.models import Tutor
 from tutors.models import AcademicRisk
+from tutors.models import AttendanceRisk
 
 # Register your models here.
 
 admin.site.register(Tutor)
 admin.site.register(AcademicRisk)
+admin.site.register(AttendanceRisk)
