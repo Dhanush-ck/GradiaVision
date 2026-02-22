2d17dc3afb6eb8b0f647070c7623e18496a6cd64
Dhanush C K <dhanushck01@gmail.com>
2026-02-22 23:53:00 +0530

    feat: add new views to the path

--- students/urls.py
+++ students/urls.py
@@ -3,8 +3,10 @@ from students import views
 
 urlpatterns = [
     path('signup/', views.signup_page, name="signup"),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard/', views.dashboard, name='student_dashboard'),
     path('upload/', views.upload, name='upload'),
     path('preview/', views.preview, name="preview"),
     path('preview/manage/', views.preview_manage, name="preview-manage"),
+    path('notification/', views.notification_manage, name='notification'),
+    path('graph/', views.graph_manage, name='graph'),
 ]
