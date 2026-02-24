ebfa56557d12deba77bd87d3b36a77f8f99b1555
Dhanush C K <dhanushck01@gmail.com>
2026-02-24 21:02:16 +0530

    feat: customize reply according to special input cases

--- chatbot/views.py
+++ chatbot/views.py
@@ -6,6 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 
 from chatbot.chatbot_manager import generate_reply
+from students.models import SemesterResult
 
 # Create your views here.
 
@@ -20,11 +21,39 @@ def chatbot(request):
 @csrf_exempt
 def reply(request):
     if request.method == "POST":
+        user = request.user.userprofile.student
         data = json.loads(request.body.decode("utf-8"))
 
         user_message = data.get("message")
 
         reply =generate_reply(user_message)
+
+        if reply == "sgpa":
+            sem_results = SemesterResult.objects.filter(student=user)
+            all_sgpa = []
+            for sem in sem_results:
+                all_sgpa.append(sem.sgpa)
+            sgpa = round(sum(all_sgpa)/len(all_sgpa), 2)
+            reply = f"Your current SGPA is {sgpa}"
+
+        if isinstance(reply, dict):
+            sem_results = SemesterResult.objects.filter(student=user)
+            all_sgpa = []
+            for sem in sem_results:
+                all_sgpa.append(sem.sgpa)
+            target_cgpa = reply['percentage'] / 10
+
+            required = round((target_cgpa * (len(all_sgpa)+1)) - sum(all_sgpa), 2)
+
+            print(all_sgpa)
+            print("CGPA", sum(all_sgpa)/len(all_sgpa))
+            print("CGPA", (sum(all_sgpa)+required)/(len(all_sgpa)+1))
+            print(required)
+        
+            if required < 10:
+                reply = f"You need {required} SGPA in next semester for achieving {target_cgpa} CGPA"
+            else:
+                reply = f"You can't achieve {target_cgpa} CGPA only through next semester"
 
         return JsonResponse({
             "reply": reply
