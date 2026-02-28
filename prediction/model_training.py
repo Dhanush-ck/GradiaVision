31a6320af8547a6d2e87a8df677e4ef759e7039c
Dhanush C K <dhanushck01@gmail.com>
2026-03-01 01:16:33 +0530

    feat: test Linear Regression and compared both models

--- prediction/model_training.py
+++ prediction/model_training.py
@@ -1,8 +1,11 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error, r2_score
+# from sklearn.linear_model import LinearRegression
 import joblib
+
+from sklearn.metrics import mean_absolute_error, r2_score
+import matplotlib.pyplot as plt
 
 
 df  = pd.read_csv('./dataset/dataset.csv')
@@ -20,6 +23,8 @@ model = RandomForestRegressor(
     random_state=42
 )
 
+# model = LinearRegression()
+
 model.fit(x_train, y_train)
 
 predictions = model.predict(x_test)
@@ -30,3 +35,13 @@ print("R2 Score: ", r2_score(y_test, predictions))
 joblib.dump(model, "random_forest.pkl")
 
 print("Model Created")
+
+# models = ['Linear Regression', 'Random Forest']
+# mae_values = [round(0.0737427702750685, 3), round(0.1215426663163808, 3)]
+# r2_values = [round(0.9941990656014914, 3), round(0.9831328551471517, 3)]
+
+# plt.bar(models, mae_values)
+# plt.xlabel("Models")
+# plt.ylabel("MAE")
+# plt.title("Model Comparison based on MAE")
+# plt.show()
