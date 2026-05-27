2f3dad3fa0422db1ecb976834be4305dda38e583
Dhanush C K <dhanushck01@gmail.com>
2026-05-27 21:20:50 +0530

    feat: add path for new views

--- tutors/urls.py
+++ tutors/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('graph/', views.tutor_graph, name='tutor_graph'),
     path('notification/add/', views.add_notification, name="add_notification"),
     path('students/', views.view_student, name='view_students'),
+    path('filter/', views.filter_student, name="filter_name"),
+    path('studentCard/', views.student_card, name="student_card"),
+    path('graphType/', views.handle_graph_type, name="handle_graph_type"),
 ]
