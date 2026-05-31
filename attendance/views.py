92b477f3d78024d9a45d6ab1de0b36d89245de19
Dhanush C K <dhanushck01@gmail.com>
2026-05-31 21:36:48 +0530

    feat: add user checking for attendance marking page

--- attendance/views.py
+++ attendance/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from students.models import Student
 
 # Create your views here.
 
 def attendance_marking(request):
+    user = request.user.userprofile.role
 
-    return render(request, 'attendance/attendance_marking.html')
+    if user != 'tutor':
+        request.session['message'] = "This webpage is only for tutors"
+        return redirect('/account/warning')
+
+    user = request.user.userprofile.tutor
+    class_charge = user.class_charge
+
+    students = Student.objects.filter(current_class=class_charge).order_by('regno')
+    if not students:
+        return render(request, 'attendance/attendance_marking.html', {
+            'message': "No records"
+        })
+        
+    data = []
+    for student in students:
+        student_obj = {
+            "name": student.username,
+            "email": student.email,
+            "rollno": int(student.regno[-2:])
+        }
+        data.append(student_obj)
+        # print(student_obj)
+
+    return render(request, 'attendance/attendance_marking.html', {
+        'data': data,
+    })
