648adf8ab9a212115fc6de01e4328d1863d495e4
Dhanush C K <dhanushck01@gmail.com>
2026-06-02 22:29:33 +0530

    feat: add path to new views

--- attendance/urls.py
+++ attendance/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from attendance import views
 
 urlpatterns = [
-    path('mark/', views.attendance_marking, name='attendance_marking')
+    path('mark/', views.attendance_marking, name='attendance_marking'),
+    path('getStudents/', views.get_students, name="get_students"),
+    path('markAttendance/', views.mark_attendance, name="mark_attendance"),
 ]
