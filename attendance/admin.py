1e9eb0c5142ae6cc2f8a78bfb87dea7beec30ac6
Dhanush C K <dhanushck01@gmail.com>
2026-08-08 00:19:53 +0530

    feat: add attendance models to the admin panel

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from attendance.models import AttendanceRecord, AttendanceSession
 
 # Register your models here.
+
+admin.site.register(AttendanceRecord)
+admin.site.register(AttendanceSession)
